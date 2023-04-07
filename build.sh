6ec792d9f34060536bea748cc7d22b15fc5871d2
Vincent Le Quec <vincent.le-quec@prestashop.com>
2023-04-07 17:55:58 +0200

    chore: some updates

--- build.sh
+++ build.sh
@@ -1,13 +1,14 @@
-#/bin/sh
+#!/bin/bash
 set -e -x
 
 function get_latest_prestashop_version {
-  curl 'http://github.com/prestashop/prestashop/blablabla'
+  return "$(curl --location --request GET 'https://api.github.com/repos/prestashop/prestashop/releases/latest' | jq -r '.tag_name')"
 }
 
+# TODO: remove regex from prestashop-versions
 function get_recommended_php_version {
   PS_VERSION=$0
-  return cat prestashop-versions.json | jq -r '.["'$PS_VERSION'"].php.recommended';
+  return "$(jq -r '.["$PS_VERSION"].php.recommended' < prestashop-versions.json)"
 }
 
 function get_tag_aliases {
@@ -24,10 +25,22 @@ LINUX_DISTRIBUTION="${LINUX_DISTRIBUTION}-debian"
 PHP_DOCKER_TAG="${PHP_VERSION}-${PHP_FLAVOUR}"
 # check here if php=$PHP_DOCKER_TAG exists
 
+# TODO:
+#  - Check if image already exists on Docker Hub before pushing ?
+#  - Check if release exists before ?
+
+## Specific version
+#DOWNLOAD_URL=$(curl --location --request GET 'https://api.github.com/repos/prestashop/prestashop/releases'| jq -r '.[] | select(.tag_name | contains("$PS_VERSION")) | .assets[] | select(.name | contains(".zip")) | .browser_download_url')
+## latest
+#DOWNLOAD_URL=$(curl --location --request GET 'https://api.github.com/repos/prestashop/prestashop/releases/latest' | jq -r '.assets[] | select(.name | contains(".zip")) | .browser_download_url')
+#curl -o ./prestashop.zip -L $DOWNLOAD_URL
+## Nightly
+#git clone 'https://github.com/PrestaShop/PrestaShop.git'
 
 TAGS=$(get_tag_aliases $PS_VERSION $PS)
 
-docker build \
+# --platform linux/amd64,linux/arm64,linux/arm
+docker buildx build \
   -f docker/${LINUX_DISTRIBUTION}-base.Dockerfile \
   --build-arg PHP_DOCKER_TAG=${PHP_DOCKER_TAG} \
   --build-arg PS_VERSION=${PS_VERSION} \
